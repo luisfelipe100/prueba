6119c4deed4376e92e0d20a581bb34d41e7ace28
Luis Felipe <actuario.felipe@gmail.com>
2017-02-22 23:29:41 -0600

    commit final

--- p4/main.js
+++ p4/main.js
@@ -1,8 +1,29 @@
+// var n = +window.prompt("Ingresa un número N");
+//
+// for (var i = 0; i < n; i++) {
+//   var stringLineaN = "";
+//   for (var j = 0; j < n; j++) {
+//     if(i===0 || i===(n-1)){
+//       stringLineaN += "*";
+//     }
+//     else {
+//       if(j===0 || j===(n-1)){
+//         stringLineaN += "*";
+//       }
+//       else {
+//         stringLineaN += " ";
+//       }
+//     }
+//   }
+//   console.log(stringLineaN);
+// }
+
+
 var n = +window.prompt("Ingresa un número N");
 
 for (var i = 0; i < n; i++) {
   var stringLineaN = "";
-  for (var j = 0; j < n; i++) {
+  for (var j = 0; j < n; j++) {
     if(i===0 || i===(n-1)){
       stringLineaN += "*";
     }
@@ -11,9 +32,9 @@ for (var i = 0; i < n; i++) {
         stringLineaN += "*";
       }
       else {
-        stringLineaN += " ";
+        stringLineaN += "_";
       }
     }
   }
-  console.log(stringLineaN);
+  document.write(stringLineaN + "<br>");
 }
